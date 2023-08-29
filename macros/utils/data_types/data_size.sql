c13683b763f9bb6bbe42915f874c450d58445aa4
nhat.nguyent <nhat.nguyent@homecredit.vn>
2023-08-29 09:53:46 +0700

    feat(macros): add duckdb data types normalization

--- macros/utils/data_types/data_size.sql
+++ macros/utils/data_types/data_size.sql
@@ -21,3 +21,7 @@
 {% macro redshift__get_column_size() %}
   {{ return(65535) }}
 {% endmacro %}
+
+{% macro duckdb__get_column_size() %}
+  {{ return(elementary.default__get_column_size()) }}
+{% endmacro %}
